75b612fb10bc02247952bc1359b5fec3a4b48298
Javier Aquino <Javier.Aquino@GMail.com>
2011-08-12 21:33:42 -0500

    Implementacion de busqueda en Editoriales

--- app/controllers/editoriales_controller.rb
+++ app/controllers/editoriales_controller.rb
@@ -1,2 +1,16 @@
 class EditorialesController < InheritedResources::Base
+  def index2
+    if params[:search]
+      @editoriales = Editorial.find(:all, :conditions => ['nombre LIKE ?', "%#{ params[:search].upcase }%" ] )
+    else
+      @editoriales = Editorial.find(:all)
+    end
+  end
+
+
+  def index
+      @editoriales = Editorial.buscar(params[:buscar]).paginate(:per_page => 5, :page => params[:page])
+  end
+
+
 end
--- app/models/editorial.rb
+++ app/models/editorial.rb
@@ -1,2 +1,19 @@
 class Editorial < ActiveRecord::Base
+  validates_presence_of :codigo, :nombre
+  validates_uniqueness_of :codigo, :nombre
+
+  scope :disponible, where( :disponible => true )
+  scope :nodisponible, where( :disponible => false )
+
+  def self.buscar(buscar)
+    if buscar
+      where('nombre LIKE ?', "%#{buscar.upcase}%")
+    else
+      scoped
+    end
+  end
+
+  def to_s
+    nombre
+  end
 end
