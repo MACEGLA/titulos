e015479fc80f6b7ac1e28af057d2469ce4ca2980
Javier Aquino H <javier.aquino@gmail.com>
2011-09-09 21:55:55 -0500

    Busqueda y ordenacion en Catgorias

--- app/controllers/categorias_controller.rb
+++ app/controllers/categorias_controller.rb
@@ -1,2 +1,25 @@
 class CategoriasController < InheritedResources::Base
+  helper_method :sort_column, :sort_direction
+
+  def collection
+    @categorias ||= end_of_association_chain.buscar(params[:buscar]).order(sort_column + ' ' + sort_direction).page(params[:page]).per(5)
+  end
+
+=begin
+  def index  
+    @titulos = Titulo.order(sort_column + ' ' + sort_direction).page(params[:page]).per(5)
+  end  
+=end
+
+  private  
+  def sort_column  
+    # params[:sort] || "codigo"  
+    Categoria.column_names.include?(params[:sort]) ? params[:sort] : "nombre"
+  end  
+    
+  def sort_direction  
+    # params[:direction] || "asc"  
+    %w[asc desc].include?(params[:direction]) ?  params[:direction] : "asc"
+  end  
+
 end
--- app/models/categoria.rb
+++ app/models/categoria.rb
@@ -1,5 +1,19 @@
 class Categoria < ActiveRecord::Base
   has_many :titulos
+  
+  validates_presence_of :nombre
+  validates_uniqueness_of :nombre
+
+  scope :disponible, where( :disponible => true )
+  scope :nodisponible, where( :disponible => false )
+
+  def self.buscar(buscar)
+    if buscar
+      where('nombre LIKE ?', "%#{buscar.upcase}%")
+    else
+      scoped
+    end
+  end
 
   def to_s
     nombre
